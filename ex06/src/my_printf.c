28ffe1a5fde23843bd3e863054ecf873fdef2923
Anton <pentatonixx1997@gmail.com>
2019-08-09 17:13:36 +0300

    Finally i made printf!!

--- ex06/src/my_printf.c
+++ ex06/src/my_printf.c
@@ -2,37 +2,37 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 char* my_itoa(int n) {
-  char *res;
-  int len = 1;
-  int sign = 1;
-  int start;
-  if ( n < 0 ) { 
-      sign = -1;
-      n = -n;
-  }
-  int nn = n;
-  while (1) {
-    n = n/10;
-    if (n == 0) { break; }    
-    len++;
-  }
-  start = (sign == -1);
-  res = malloc(len + start + 1);
-  if (start) { res[0] = '-';}
-  for (int i = len + start - 1; i >= start; i--) {
-    res[i] = (nn % 10) +'0';
-    nn = nn/10;
-  }
-  res[len+start] = '\0';
-  return res;
+    char *res;
+    int len = 1;
+    int sign = 1;
+    int start;
+    if ( n < 0 ) { 
+        sign = -1;
+        n = -n;
+    }
+    int nn = n;
+    while (1) {
+        n = n/10;
+        if (n == 0) { break; }    
+        len++;
+    }
+    start = (sign == -1);
+    res = malloc(len + start + 1);
+    if (start) { res[0] = '-';}
+    for (int i = len + start - 1; i >= start; i--) {
+        res[i] = (nn % 10) +'0';
+        nn = nn/10;
+    }
+    res[len+start] = '\0';
+    return res;
 }
 
-int my_atoi(const char *s) {
+int my_atoi(const char *s, int cnt) {
     int res = 0;
     int i = 0;
-    while (s[i] != '\0') {
+    while ( i < cnt) {   
         res = 10*res + s[i] - '0';
 	i++;
     }
@@ -45,109 +45,81 @@ unsigned int my_strlen(char *str) {
     return n;
 }
 
-int get_pad (const char *nptr) {
+int read_pad (const char *nptr) {
     int i, n = 0;
     for (i = 0; nptr[i]>='0' && nptr[i]<='9'; i++) { n++; }
     return n;
 }
 
-int str_print(char *str) {
+void str_print(char *str) {
     while (*str != '\0') {
-       write(1,str,1);
-       str++;
+       write(1,str++,1);
     }
-    return 0;
 }
 
 void add_pad(char c, int cnt) {
    for (; cnt > 0; cnt--) { write(1,&c,1); }
 }
 
-int determination(const char *type, va_list *argptr)
-
-{
-  int flag=0;
-    char *ptrOnArg;
-    int width=0;
-    int pad_to_char;
-    int pad_to_string;
-    if (*type=='0')
-    {
-      type++;
-      flag=1;
+int print_format(const char *s, va_list *args) {
+    if (*s == '%') {
+        write(1,"%",1);
+	return 2;
     }
-    switch(*type)
-    {
-     case 's':
-     str_print(va_arg(*argptr, char *));
-     return 2;
-
-     case '%':
-     write(1,"%",1);
-     return 2;
-
-     case 'd':
-     ptrOnArg=my_itoa(va_arg(*argptr,int ));
-     str_print(ptrOnArg);
-     return 2;
-
-
-
-     default:
-     width=my_atoi(type);
-     pad_to_char=get_pad(type);
-     if (*(type+pad_to_char)=='d')
-     {
-       ptrOnArg=my_itoa(va_arg(*argptr,int ));
-       pad_to_string=width- my_strlen(ptrOnArg);
-     }
-     else
-     {
-     ptrOnArg=va_arg(*argptr,char *);
-     pad_to_string=width- my_strlen(ptrOnArg);
-     }
-     if (flag==1)
-     {
-      add_pad('0',pad_to_string);
-     }
-     else
-     {
-       add_pad(' ',pad_to_string);
-     }
-      str_print(ptrOnArg);
-     return (pad_to_char+flag+2);
+    if (*s == 's') {
+        str_print(va_arg(*args,char *));
+        return 2;
     }
-    return 0;
+    if (*s == 'd') {
+        str_print(my_itoa(va_arg(*args,int)));
+        return 2;
+    }
+    int flag = 0;
+    if (*s == '0') {
+	s++;
+	flag = 1;
+    }
+    int shift = read_pad(s);
+    int pad = my_atoi(s,shift);
+    char *str;
+    int len;
+    if (*(s + shift) == 'd') {    
+        str = my_itoa(va_arg(*args,int));
+        len = my_strlen(str);
+        pad = pad - len;
+	if (flag) {
+	add_pad('0',pad);
+	} else { add_pad(' ',pad); }
+	str_print(str); 
+	return shift + 2 + flag;
+    }
+    if (*(s + shift) == 's') {
+        str = va_arg(*args, char *);
+	len = my_strlen(str);
+	add_pad(' ',pad - len);
+	str_print(str);
+    }
+    return shift + 2;
 }
 
-
-int my_printf(const char *format, ...)
-{
-
- int a=0;
- va_list argptr;
- va_start(argptr,format);
- while (format[a]!='\0')
-  {
-    if (format[a]=='%')
-    {
-      a=a+determination(format+a+1, &argptr);
-    }
-    if (format[a]=='\n')
-    {
-      break;
-    }
-    else
-    {
-      write (1,format+a,1);
-      a++;
-    }
-  }
+int my_printf(const char *format, ...) {
+    va_list args;
+    va_start(args,format);
+    const char *i = format; 
+    while (*i != '\0') {
+        if (*i == '%') {
+	    i = i + print_format(i+1,&args);
+	}
+	else { 
+	    if (*i == '\n') {
+	        break;
+	    } else {
+	        write(1,i++,1);
+	    }
+	}
+    }  
     write(1,"\n",1);
-   va_end(argptr);
-   return a;
-}
-
-
-
+    va_end(args);
+    return 0; 
+}	
 
